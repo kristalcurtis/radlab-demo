3df9606c6d9bc85f2fa528cde2d97980331f3917
ksauer <ksauer@w8722fd0x91.(none)>
2010-12-17 19:14:15 -0800

    fixed visualization code so it can deal with elapsed time data

--- scads-visualization/Code/visualization.R
+++ scads-visualization/Code/visualization.R
@@ -1,4 +1,6 @@
-miniTraceData = as.data.frame(read.csv("~/Desktop/miniTraceData.csv"))
+#miniTraceDataFilename = "~/Desktop/radlab-demo/scads-visualization/Data/Processed/miniTraceData.csv"
+miniTraceDataFilename = "~/Desktop/radlab-demo/scads-visualization/Data/Processed/miniTraceDataWithElapsedTimes.csv"
+miniTraceData = as.data.frame(read.csv(miniTraceDataFilename))
 
 colnames(miniTraceData)
 
@@ -18,4 +20,21 @@ operationType = "getQuery100000"
 getElapsedTime = function(traceData, operationType) {
 	times = traceData$timestamp[which(traceData$operationType == operationType)]
 	
+}
+# UPDATE:  no longer necessary -- did this in Scala
+
+# next: visualize
+
+query1Data = miniTraceData[1:7,]
+max(query1Data$timestamp + query1Data$elapsedTime)
+
+plot(c(0,750000), c(0,100), col=0, xlab="Time (ns)", ylab="")
+lines(c(query1Data$timestamp[query1Data$type == "query"], query1Data$elapsedTime[query1Data$type == "query"]), c(75, 75), lw=2, col="red")
+
+for (i in 1:6) {
+	if (query1Data$type[i] == "iterator") {
+		lines(c(query1Data$timestamp[i], query1Data$timestamp[i] + query1Data$elapsedTime[i]), c(50, 50), lw=2, col="blue")
+	} else if (query1Data$type[i] == "message") {
+		lines(c(query1Data$timestamp[i], query1Data$timestamp[i] + query1Data$elapsedTime[i]), c(25, 25), lw=2, col="purple")
+	}
 }
