b4065d82442d2242b18cd871e375bc1ba0f6b64c
ksauer <ksauer@w8722fd0x91.(none)>
2010-12-21 12:13:22 -0800

    added color coding for iterator functions

--- scads-visualization/Code/visualization-functions.R
+++ scads-visualization/Code/visualization-functions.R
@@ -38,19 +38,22 @@ plotAllEventsForGivenQuery = function(queryData) {
 	xlimMax = max(queryData$timestamp + queryData$elapsedTime)
 
 	par(yaxt="n", mar=c(5,2,4,2))
-	plot(c(xlimMin, xlimMax), c(0,100), col=0, xlab="Time (ns)", ylab="", main=paste("Visualization of", queryId, "query"))
+	plot(c(xlimMin, xlimMax), c(0,120), col=0, xlab="Time (ns)", ylab="", main=paste("Visualization of", queryId, "query"))
+
+	colorVectorForIteratorEvents = getColorVectorForIteratorEvents(queryData)
 
 	for (i in 1:nrow(queryData)) {
 		if (queryData$type[i] == "query") {
 			lines(getEventStartAndEndTimes(queryData[i,]), c(75, 75), lw=2, col="red")
 		} else if (queryData$type[i] == "iterator") {
-			lines(getEventStartAndEndTimes(queryData[i,]), c(50, 50), lw=2, col="blue")
+			lines(getEventStartAndEndTimes(queryData[i,]), c(50, 50), lw=2, col=colorVectorForIteratorEvents[i])
 		} else if (queryData$type[i] == "message") {
 			lines(getEventStartAndEndTimes(queryData[i,]), c(25, 25), lw=2, col="purple")
 		}
 	}
 
-	legend("topright", legend=c("query", "iterator", "message"), col=c("red", "blue", "purple"), lw=2)
+	#legend("topright", legend=c("query", "iterator", "message"), col=c("red", "blue", "purple"), lw=2)
+	createLegendForPlotWithAllQueryEvents()
 }
 
 getQueryIdFromQueryData = function(queryData) {
@@ -59,4 +62,44 @@ getQueryIdFromQueryData = function(queryData) {
 
 getEventStartAndEndTimes = function(event) {
 	return(c(event$timestamp, event$timestamp + event$elapsedTime))
+}
+
+getColorVectorForIteratorEvents = function(queryData) {
+	colorVectorForIteratorEvents = vector(length=nrow(queryData))
+	colorMap = getMapFromIteratorFunctionsToColors()
+	
+	for (i in 1:nrow(queryData)) {
+		if (queryData$type[i] == "iterator") {
+			if (length(grep("open", queryData$id[i])) == 1) {
+				colorVectorForIteratorEvents[i] = colorMap[1,2]
+			} else if (length(grep("hasNext", queryData$id[i])) == 1) {
+				colorVectorForIteratorEvents[i] = colorMap[2,2]
+			} else if (length(grep("next", queryData$id[i])) == 1) {
+				colorVectorForIteratorEvents[i] = colorMap[3,2]
+			} else if (length(grep("close", queryData$id[i])) == 1) {
+				colorVectorForIteratorEvents[i] = colorMap[4,2]
+			}
+		}
+	}
+	
+	return(colorVectorForIteratorEvents)
+}
+
+getMapFromIteratorFunctionsToColors = function() {
+	map = matrix(nrow=4, ncol=2)
+	colnames(map) = c("function", "color")
+	
+	map[1,] = c("iterator:open", "blue")
+	map[2,] = c("iterator:hasNext", "cyan")
+	map[3,] = c("iterator:next", "olivedrab")
+	map[4,] = c("iterator:close", "magenta")
+	
+	return(map)
+}
+
+createLegendForPlotWithAllQueryEvents = function() {
+	mapFromIteratorFunctionsToColors = getMapFromIteratorFunctionsToColors()
+	
+	legend("topright", legend=c("query", mapFromIteratorFunctionsToColors[,"function"], "message"), col=c("red", mapFromIteratorFunctionsToColors[,"color"], "purple"), lw=2)
+	
 }
