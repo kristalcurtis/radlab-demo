c33af4060c76135bdd3afa78c3f92143c2442993
ksauer <ksauer@w8722fd0x91.(none)>
2011-01-31 15:01:09 -0800

    plotting cardinality vs. latency for a few different query datasets

--- scads-visualization/Code/exploreCardinality.R
+++ scads-visualization/Code/exploreCardinality.R
@@ -23,29 +23,66 @@ for (i in unique(queryData$rangeLength)) {
 	print(paste("99th=", quantile(queryDataInMs$elapsedTime[which(queryDataInMs$rangeLength == i)], 0.99)))
 }
 
-visualizeLatencyQuantilesVsCardinality = function(queryData) {
-	median = getLatencyQuantileForEachCardinalityValue(queryData, 0.5)
-	q90 = getLatencyQuantileForEachCardinalityValue(queryData, 0.9)
-	q99 = getLatencyQuantileForEachCardinalityValue(queryData, 0.99)
-	
-	plot(getCardinalityValues(queryData), q99)
-}
 
-getCardinalityValues = function(queryData) {
-	return(unique(queryData$rangeLength))	
-}
-
-getLatencyQuantileForEachCardinalityValue = function(queryData, quantile) {
-	cardinalityValues = getCardinalityValues(queryData)
-	numCardinalityValues = length(cardinalityValues)
-	
-	quantiles = vector(length=numCardinalityValues)
-	
-	for (i in cardinalityValues) {
-		quantiles[i] = quantile(queryData$elapsedTime[which(queryDataInMs$rangeLength == i)], quantile)
-	}
-	
-	return(quantiles)
-}
-
+source("exploreCardinalityFunctions.R")
 visualizeLatencyQuantilesVsCardinality(queryDataInMs)
+
+getLatencyQuantileForEachCardinalityValue(queryDataInMs, 0.5)
+
+getCardinalityValues(queryDataInMs)
+
+
+# try again with a different run
+queryFilename = "/Users/ksauer/Desktop/piqltrace.avro-1296073176519.csv"
+queryData = as.data.frame(read.csv(queryFilename))
+queryDataInMs = convertTimesFromNanosecondsToMilliseconds(queryData)
+source("exploreCardinalityFunctions.R")
+visualizeLatencyQuantilesVsCardinality(queryDataInMs)
+
+
+cardinalityValues = getCardinalityValues(queryDataInMs)
+
+for (i in 1:length(cardinalityValues)) {
+	print(cardinalityValues[i])
+	print(summary(getDataForGivenCardinalityValue(queryDataInMs, cardinalityValues[i])))
+}
+
+
+# try again with different run -- this time, we have a larger cardinality at the end
+queryFilename = "/Users/ksauer/Desktop/piqltrace.avro-1296165307356.csv"
+queryData = as.data.frame(read.csv(queryFilename))
+queryDataInMs = convertTimesFromNanosecondsToMilliseconds(queryData)
+
+source("exploreCardinalityFunctions.R")
+
+pdf("~/Desktop/piqltrace.avro-1296165307356.pdf")
+visualizeLatencyQuantilesVsCardinality(queryDataInMs)
+dev.off()
+
+summary(getDataForGivenCardinalityValue(queryDataInMs, 10))
+
+
+
+# making plot for join query
+setwd("/Users/ksauer/Desktop/radlab-demo/scads-visualization/Code")
+source("visualization-functions.R")
+source("exploreCardinalityFunctions.R")
+
+queryFilename = "/Users/ksauer/Desktop/piqltrace.avro-1296262054148.csv"
+
+queryData = as.data.frame(read.csv(queryFilename))
+queryDataInMs = convertTimesFromNanosecondsToMilliseconds(queryData)
+
+pdf(paste(queryFilename, ".pdf", sep=""))
+visualizeLatencyQuantilesVsCardinality(queryDataInMs)
+dev.off()
+
+# try again with reversed cardinality list
+queryFilename="/Users/ksauer/Desktop/piqltrace.avro-1296264746793.csv"
+
+queryData = as.data.frame(read.csv(queryFilename))
+queryDataInMs = convertTimesFromNanosecondsToMilliseconds(queryData)
+
+pdf(paste(queryFilename, ".pdf", sep=""))
+visualizeLatencyQuantilesVsCardinality(queryDataInMs)
+dev.off()
